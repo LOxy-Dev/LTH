0abac35897a15a0cb1842f20b7e42213b7ff2df8
LOxyDev <40117765+LOxyDev@users.noreply.github.com>
2020-10-29 01:46:08 +0100

    Adding Main Window

---
+++ LTHWindow/Windows/Main/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LTHWindow.Windows.Main
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
